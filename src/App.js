abcfa472bbfa97b53ef0d5f313de4ab0f0b3eccd
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-03 11:17:32 -0600

    Learn React chapter 1 lesson complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Greeting } from './Greeting';
+import { PeopleList } from './PeopleList';
 import './App.css';
 
+const people = [{
+  name: 'John',
+  age: 40,
+  hairColor: 'brown',
+}, {
+  name: 'Freyja',
+  age: 4,
+  hairColor: 'white',
+}, {
+  name: 'Rocky',
+  age: 2,
+  hairColor: 'brown',
+}];
+
 function App() {
+  let adjective = 'sweet';
+  let url = 'https://reactjs.org';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ color: 'red', fontSize: '96px' }}>Big Red Text</p>
+        <Greeting name ="Freyja" numberOfMessages={10}/>
+        <PeopleList people={people} />
+        <button onClick={() => alert('Hello!')}>Click Me!</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is so {adjective}!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href= {url}
           target="_blank"
           rel="noopener noreferrer"
         >
